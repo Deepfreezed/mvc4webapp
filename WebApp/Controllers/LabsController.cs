4948cb582ed3ed067fe3b64cc4531b780ff81af7
jcsamarasekera <janith6dr@hotmail.com>
2013-02-07 16:48:28 -0600

    Initial commit

---
+++ WebApp/Controllers/LabsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class LabsController : Controller
+    {
+		//
+		// POST: /Labs/Lab3
+		public ActionResult Lab3()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public ActionResult Lab3(LabModels model)
+		{
+			
+
+			return View(model);
+			//return PartialView("_ModulusSumPartial", model);
+		}
+    }
+}
---
+++ WebApp/Models/LabModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+	public class LabModels
+	{
+		[Required]
+		[Display(Name = "User name")]
+		[StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+		public string UserName { get; set; }
+
+		[Required]
+		[Display(Name = "User ID")]
+		[RegularExpression(@"[0-9]+", ErrorMessage = "{0} must be a Number.")]
+		public string UserID { get; set; }
+
+		public int ModulusSum 
+		{ 
+			get
+			{
+				return DigitSum % 100;
+			}
+		}
+
+		public int DigitSum
+		{
+			get
+			{
+				return UserID.Sum(c => c - '0');
+			}
+		}
+	}
+}
