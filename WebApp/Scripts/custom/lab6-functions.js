8a26d597f286f9b1ac2319c1ecaab36910cf91b9
jcsamarasekera <janith6dr@hotmail.com>
2013-03-22 11:30:57 -0500

    Lab 6 Update 1

--- WebApp/Scripts/custom/lab6-functions.js
+++ WebApp/Scripts/custom/lab6-functions.js
@@ -9,17 +9,62 @@
 		return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function refreshPage() {
-	var style = getParameterByName("style");
+function updateURLParameter(url, param, paramVal) {
+	var TheAnchor = null;
+	var newAdditionalURL = "";
+	var tempArray = url.split("?");
+	var baseURL = tempArray[0];
+	var additionalURL = tempArray[1];
+	var temp = "";
 
+	if (additionalURL) {
+		var tmpAnchor = additionalURL.split("#");
+		var TheParams = tmpAnchor[0];
+		TheAnchor = tmpAnchor[1];
+		if (TheAnchor)
+			additionalURL = TheParams;
+
+		tempArray = additionalURL.split("&");
+
+		for (i = 0; i < tempArray.length; i++) {
+			if (tempArray[i].split('=')[0] != param) {
+				newAdditionalURL += temp + tempArray[i];
+				temp = "&";
+			}
+		}
+	}
+	else {
+		var tmpAnchor = baseURL.split("#");
+		var TheParams = tmpAnchor[0];
+		TheAnchor = tmpAnchor[1];
+
+		if (TheParams)
+			baseURL = TheParams;
+	}
+
+	if (TheAnchor)
+		paramVal += "#" + TheAnchor;
+
+	var rows_txt = temp + "" + param + "=" + paramVal;
+	return baseURL + "?" + newAdditionalURL + rows_txt;
+}
+
+function refreshPage() {
+	var style = parseInt(getParameterByName("style"));
+		
 	if (style != null && style >= 3) {
 		style = 1;
 	}
-	else {
+	else if (style != null && (style < 3 && style > 0)) {
 		style = style + 1;
 	}
+	else {
+		style = 1;
+	}
 
-	window.location.href = window.location.href + "?style=" + style;
+	var newUrl = updateURLParameter(window.location.href, "style", style);
+
+	window.location.href = newUrl;
 }
 
 $(document).ready(function () {
